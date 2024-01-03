78457ce3e87936cfe674335946b1b09f42e9f013
Huzaifah Dzulkifli <h.BinDzulkifli@gambit-finance.com>
2024-01-03 11:30:59 +0800

    Implement correlation ID and Nlog

---
+++ Cdn.Freelance.Api/LayoutRenderers/CorrelationIdLayoutRenderer.cs
@@ -0,0 +1,29 @@
+﻿using NLog.LayoutRenderers;
+using NLog;
+using System.Text;
+using Cdn.Freelance.Api.Middlewares;
+using NLog.Web.LayoutRenderers;
+
+namespace Cdn.Freelance.Api.LayoutRenderers
+{
+    /// <summary>
+    /// Represents a renderer that appends the CorrelationId in NLog logs if it exists.
+    /// </summary>
+    [LayoutRenderer("correlation-id")]
+    public class CorrelationIdLayoutRenderer : AspNetLayoutRendererBase
+    {
+        /// <summary>
+        /// Appends the CorrelationId in NLog logs if it exists.
+        /// </summary>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var correlationId = HttpContextAccessor.HttpContext?.Request.Headers[CorrelationIdMiddleware.CorrelationIdHeader];
+            if (string.IsNullOrWhiteSpace(correlationId))
+            {
+                correlationId = HttpContextAccessor?.HttpContext?.Response.Headers[CorrelationIdMiddleware.CorrelationIdHeader];
+            }
+
+            builder.Append(correlationId);
+        }
+    }
+}
---
+++ Cdn.Freelance.Api/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Cdn.Freelance.Api.Middlewares
+{
+    /// <summary>
+    /// The correlationId middleware. Propagates the current correlationId or generates a new one if it doesn't exist onto request and response.
+    /// </summary>
+    public class CorrelationIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public const string CorrelationIdHeader = "X-Request-ID";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CorrelationIdMiddleware" /> class.
+        /// </summary>
+        /// <param name="next">The next RequestDelegate to execute after this one.</param>
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// Invokes the CorrelationIdMiddleware to add or propagate the correlationId onto request and response headers.
+        /// </summary>
+        /// <param name="context"></param>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            bool correlationIdExists = context.Request.Headers.TryGetValue(CorrelationIdHeader, out StringValues correlationIds);
+            var correlationId = correlationIds.FirstOrDefault() ?? Guid.NewGuid().ToString();
+
+            if (!correlationIdExists)
+                context.Response.Headers.Append(CorrelationIdHeader,correlationId);
+
+            await _next(context);
+        }
+    }
+}
